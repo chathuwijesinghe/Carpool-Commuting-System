533812e99b662d3dc43db26d0e79120230702019
Nishen Peiris <nishenkpeiris@gmail.com>
2017-04-01 10:25:14 +0530

    Add database configuration file

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: nishen
+ * Date: 3/29/17
+ * Time: 10:31 AM
+ */
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'my8@aire');
+define('DB_DATABASE', 'carpool_commuting_system');
+
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE); // Create connection
