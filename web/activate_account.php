2662334c4f21b5848d8fe1b5a7a03915bd9950ee
Nishen Peiris <nishenkpeiris@gmail.com>
2017-04-02 07:58:26 +0530

    Complete user account verification

---
+++ web/activate_account.php
@@ -0,0 +1,56 @@
+<?php
+
+require '../config/config.php';
+
+session_start();
+$username = $_SESSION["username"];
+$email = $_SESSION["email"];
+
+$activation_code = $activation_code_error = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $activation_code = $_POST["activation_code"];
+    // compare the activation code entered by the user, with the activation code stored in the database
+    $sql = "SELECT * FROM inactive_user WHERE username = '$username' AND activation_code = '$activation_code'";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        /* activation code entered by user is correct */
+        // move the user from inactive_users to users table
+        while ($row = $result->fetch_assoc()) {
+            $password = $row["password"];
+        }
+
+        /* delete the entry from inactive_user table */
+        $sql = "DELETE FROM inactive_user WHERE username = '$username' AND activation_code = '$activation_code'; ";
+        if (mysqli_query($conn, $sql)) {
+            /* create an entry in user table */
+            $sql = "INSERT INTO user (username, password, email) VALUES ('$username', '$password', '$email'); ";
+            if (mysqli_query($conn, $sql)) {
+                header("Location: home.php");
+                die();
+            } else {
+                $internal_error = "Error: " . $sql . " < br>" . mysqli_error($conn);
+            }
+        } else {
+            $internal_error = "Error: " . $sql . " < br>" . mysqli_error($conn);
+        }
+    } else {
+        $activation_code_error = "Activation code is invalid . Please check the activation code you entered . ";
+    }
+}
+?>
+
+<html>
+<body>
+<span><?php echo $internal_error; ?></span><br/>
+<p>An email with your activation code has been sent to <?php echo $email ?></p>
+<!-- htmlspecialchars is used to protect against XSS attacks -->
+<form action=" <?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+    <label>Activation code:</label>
+    <input type="text" name="activation_code" value="<?php echo htmlspecialchars($activation_code); ?>"
+           autofocus="autofocus"/>
+    <span><?php echo $activation_code_error; ?></span><br/>
+    <input type="submit" value=" Submit "/>
+</form>
+</body>
+</html>
--- web/register.php
+++ web/register.php
@@ -1,95 +1,127 @@
 <?php
+
 require '../config/config.php';
 
-$username = $password = $re_password = $email = $activationCode = $username_error = $password_error = $re_password_error = $email_error = "";
+session_start();
+$_SESSION["username"] = $_SESSION["email"] = "";
+
+$username = $password = $re_password = $email = $activationCode = $username_error = $password_error = $re_password_error = $email_error = $internal_error = "";
 $execute = true;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    /* validate all inputs */
     // validate the username
     $username = $_POST["username"];
     if (empty($username)) {
         $username_error = "Username can't be empty.";
+        $execute = false;
     } else if (strlen($username) < 4) {
         $username_error = "Username should contain at least 4 chars.";
+        $execute = false;
+    } else {
+        $_SESSION["username"] = $username;
     }
 
     // validate the password
     $password = $_POST["password"];
     if (empty($password)) {
         $password_error = "Password can't be empty.";
+        $execute = false;
     } else if (strlen($password) < 4) {
         $password_error = "Password should contain at least 4 chars.";
-    } else {
-        // validate repeated password
-        $re_password = $_POST["re_password"];
-        if (strcmp($re_password, $password) !== 0) {
-            $re_password_error = "Password should match with the repeated password.";
-        }
+        $execute = false;
+    }
+
+    // validate repeated password
+    $re_password = $_POST["re_password"];
+    if (empty($re_password)) {
+        $re_password_error = "Repeated password can't be empty.";
+        $execute = false;
+    } else if (strlen($re_password) < 4) {
+        $re_password_error = "Repeated password should contain at least 4 chars.";
+        $execute = false;
+    } else if (strcmp($re_password, $password) !== 0) {
+        $re_password_error = "Password should match with the repeated password.";
+        $execute = false;
     }
 
     // validate the email
     $email = $_POST["email"];
     if (empty($email)) {
         $email_error = "Email can't be empty.";
+        $execute = false;
     } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $email_error = "Email you entered is invalid.";
-    }
-
-    /* if all inputs are valid */
-    //generate the 5 char activation activation code
-    $activationCode = substr(uniqid('', true), -5);
-
-    $sql = "SELECT * FROM users WHERE username = '$username'";
-    $result = $conn -> query($sql);
-    if ($result -> num_rows > 0) {
-        $username_error = "Username already exists. Please choose another.";
         $execute = false;
-    }
-
-    $sql = "SELECT * FROM users WHERE email = '$email'";
-    $result = $conn -> query($sql);
-    if ($result -> num_rows > 0) {
-        $email_error = "This email is associated with an account. Please enter the another email.";
-        $execute = false;
-    }
-
-    $sql = "SELECT * FROM inactive_users WHERE username = '$username'";
-    $result = $conn -> query($sql);
-    if ($result -> num_rows > 0) {
-        $username_error = "An account has been requested with this username. Please choose another.";
-        $execute = false;
-    }
-
-    $sql = "SELECT * FROM inactive_users WHERE email = '$email'";
-    $result = $conn -> query($sql);
-    if ($result -> num_rows > 0) {
-        $email_error = "An account has been requested for this email. Please enter the another email.";
-        $execute = false;
-    }
-    
-    if($execute == true) {
-          // insert user into inactive_users table - need account verification
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    
-          $sql = "INSERT INTO inactive_users (username, password, email, activation_code)
-VALUES ('$username', '$password', '$email', '$activationCode')";
-
-    if (mysqli_query($conn, $sql)) {
-        header("Location: activate_account.php");
-	die();
     } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        $_SESSION["email"] = $email;
     }
 
-    mysqli_close($conn);
+    /* if all inputs are valid, then */
+    if ($execute) {
+        //generate the 5 char activation activation code
+        $activationCode = substr(uniqid('', true), -5);
+
+        /* perform validations related to database */
+        $sql = "SELECT * FROM user WHERE username = '$username'";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            $username_error = "Username already exists. Please choose another.";
+            $execute = false;
+        }
+
+        $sql = "SELECT * FROM user WHERE email = '$email'";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            $email_error = "This email is associated with an account. Please enter the another email.";
+            $execute = false;
+        }
+
+        $sql = "SELECT * FROM inactive_user WHERE username = '$username'";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            $username_error = "An account has been requested with this username. Please choose another.";
+            $execute = false;
+        }
+
+        $sql = "SELECT * FROM inactive_user WHERE email = '$email'";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            $email_error = "An account has been requested for this email. Please enter the another email.";
+            $execute = false;
+        }
+    }
+
+    /* then, if all database constrains are preserved */
+    if ($execute) {
+        // insert user into inactive_users table - need account verification
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        $sql = "INSERT INTO inactive_user (username, password, email, activation_code)
+VALUES ('$username', '$password', '$email', '$activationCode')";
+        if (mysqli_query($conn, $sql)) {
+            // send an email with the activation code
+            /*            $retval = mail("nishenkpeiris@gmail.com", "Activation Code", $activationCode);
+                        if ($retval == true) {
+                            echo "Message sent successfully...";
+                        } else {
+                            echo "Message could not be sent...";
+                        }*/
+
+            // redirect to account activation
+            header("Location: activate_account.php");
+            die();
+        } else {
+            $internal_error = "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+
+        mysqli_close($conn);
     }
 }
-
 ?>
 
-<? xml version = "1.0" encoding = "UTF-8"?>
 <!DOCTYPE html
         PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -99,16 +131,26 @@ VALUES ('$username', '$password', '$email', '$activationCode')";
     <title>Create a member's account</title>
 </head>
 <body>
-<form id="register_form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+<span><?php echo $internal_error; ?></span><br/>
+<!-- htmlspecialchars is used to protect against XSS attacks -->
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
     <!--<div class="g-recaptcha" data-sitekey="6Lce6hoUAAAAAOh37uDcc1MVtv6qY35GLF46FNJ2"></div>-->
-    <label>Username :</label><input type="text" name="username" value="<?php echo htmlspecialchars($username); ?>"
-                                    autofocus="autofocus"/><span><?php echo $username_error; ?></span><br/>
-    <label>Password :</label><input type="password" name="password" value="<?php echo htmlspecialchars($password); ?>"/><span><?php echo $password_error; ?></span><br/>
-    <label>Repeat password :</label><input type="password" name="re_password"
-                                           value="<?php echo htmlspecialchars($re_password); ?>"/><span><?php echo $re_password_error; ?></span><br/>
-    <label>Email :</label><input type="email" name="email"
-                                 value="<?php echo htmlspecialchars($email); ?>"/><span><?php echo $email_error; ?></span><br/>
-    <input type="submit" value=" Submit "/><input type="reset"/><br/>
+    <label>Username :</label>
+    <input type="text" name="username" value="<?php echo htmlspecialchars($username); ?>"
+           autofocus="autofocus"/>
+    <span><?php echo $username_error; ?></span><br/>
+    <label>Password :</label>
+    <input type="password" name="password" value="<?php echo htmlspecialchars($password); ?>"/>
+    <span><?php echo $password_error; ?></span><br/>
+    <label>Repeat password :</label>
+    <input type="password" name="re_password"
+           value="<?php echo htmlspecialchars($re_password); ?>"/>
+    <span><?php echo $re_password_error; ?></span><br/>
+    <label>Email :</label>
+    <input type="email" name="email"
+           value="<?php echo htmlspecialchars($email); ?>"/>
+    <span><?php echo $email_error; ?></span><br/>
+    <input type="submit" value=" Submit "/>
 </form>
 </body>
 </html>
