d75d074357bd0f74b2f65539598a1b5cd6f58241
Nishen Peiris <Nishen Peiris>
2017-04-07 19:56:49 +0530

    Complete the find ride functionality

--- web/home.php
+++ web/home.php
@@ -1,5 +1,3 @@
-<?php require_once('header.php'); ?>
-<?php require_once('header_navbar.php'); ?>
 <?php
 /**
  * Created by IntelliJ IDEA.
@@ -18,6 +16,8 @@ $internal_error = $from = $from_error = $to = $to_error = "";
  * Time: 17:52
  */
 require_once '../config/config.php';
+require_once 'header.php';
+require_once 'header_navbar.php';
 
 $internal_error = $from = $from_error = $to = $to_error = $start_date = $start_date_error = "";
 $execute = true;
@@ -61,99 +61,113 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         setcookie("from", $from, time() + (86400 * 30), "/"); // 86400 = 1 day
         setcookie("to", $to, time() + (86400 * 30), "/"); // 86400 = 1 day
 
-        $sql = "SELECT * FROM post";
-        $result = $conn->query($sql);
-        if ($result->num_rows > 0) {
-            // output data of each row
-            while ($row = $result->fetch_assoc()) {
-                echo "Available no. of seats: " . $row["available_seats"] . " - Phone number: " . $row["phone_number"] . " - Comment " . $row["comment"] . "<br>";
-            }
-        } else {
-            echo "No rides available. Come back later.";
-        }
+        // find recurring rides
+        $sql = "SELECT * FROM post WHERE departure_point = '$from' AND arrival_point = '$to' AND frequency = 'recurring'";
+        $results_recurring = $conn->query($sql);
+
+        // find one time rides
+        $sql = "SELECT * FROM post WHERE departure_point = '$from' AND arrival_point = '$to' AND frequency = 'one_time'";
+        $results_one_time = $conn->query($sql);
     }
 }
 ?>
-
 
     <div class="container main_home_container">
         <div class="row">
             <div class="quote"> Rideshare from Anywhere to Everywhere</div>
         </div>
         <div class="row">
-           <div class="col-sm-6 left_form">
-               <div class="find_a_ride_wrp">
-                   <h3>Find a Ride</h3>
-                   <span><?php echo $internal_error; ?></span><br/>
-                   <!-- htmlspecialchars is used to protect against XSS attacks -->
-                   <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                       <div class="input_wrp">
-                           <input type="text" placeholder="From" name="from" value="<?php echo htmlspecialchars($from); ?>"
-                                  autofocus="autofocus"><i class="fa fa-map-marker" aria-hidden="true"></i>
-                       </div>
-                       <span><?php echo $from_error; ?></span><br/>
-                       <div class="input_wrp">
-                           <input type="text" name="to" placeholder="To" value="<?php echo htmlspecialchars($to); ?>"/><i
-                               class="fa fa-map-marker" aria-hidden="true"></i>
-                       </div>
-                       <span><?php echo $to_error; ?></span><br/>
-                       <div class="input_wrp">
-                           <input type="date" name="start_date" placeholder="Start Date" min="<?php echo date('Y-m-d'); ?>"
-                                  value="<?php echo htmlspecialchars($start_date); ?>"/><br/>
-                       </div>
-                       <div class="search_btn">
-                           <!--               <input type="submit" value="Search"/>-->
-                           <button type="submit" value="Search"><i class="fa fa-search" aria-hidden="true"></i> SEARCH</button>
-                       </div>
-                   </form>
-               </div>
-           </div>
+            <div class="col-sm-6 left_form">
+                <div class="find_a_ride_wrp">
+                    <h3>Find a Ride</h3>
+                    <span><?php echo $internal_error; ?></span><br/>
+                    <!-- htmlspecialchars is used to protect against XSS attacks -->
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="input_wrp">
+                            <input type="text" placeholder="From" name="from"
+                                   value="<?php echo htmlspecialchars($from); ?>"
+                                   autofocus="autofocus"><i class="fa fa-map-marker" aria-hidden="true"></i>
+                        </div>
+                        <span><?php echo $from_error; ?></span><br/>
+                        <div class="input_wrp">
+                            <input type="text" name="to" placeholder="To" value="<?php echo htmlspecialchars($to); ?>"/><i
+                                    class="fa fa-map-marker" aria-hidden="true"></i>
+                        </div>
+                        <span><?php echo $to_error; ?></span><br/>
+                        <div class="input_wrp">
+                            <input type="date" name="start_date" placeholder="Start Date"
+                                   min="<?php echo date('Y-m-d'); ?>"
+                                   value="<?php echo htmlspecialchars($start_date); ?>"/><br/>
+                        </div>
+                        <div class="search_btn">
+                            <button type="submit" value="Search"><i class="fa fa-search" aria-hidden="true"></i> SEARCH
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-
-<!--        <div class="quote"> Rideshare from Anywhere to Everywhere</div>-->
     </div>
     <div class="container results_content">
         <div class="search_results">
-            <div class="alert alert-danger" role="alert">
-                <strong>Oh Sorry!</strong> No rides available. Come back later...:)
-            </div>
-            <div>
-                <h3>Available Rides</h3>
-                <hr>
-            </div>
-
-            <div class="panel-group available_rides_panel">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Colombo to Negambo</div>
-                    <div class="panel-body">
-                        <div class="col-sm-8">
-                            <p>I'm heading Yatalamatta to Galle. If anyone willing to join my ride, please contact me.</p>
-                            <div class="col-sm-6">
-                                <label>Departure point:</label><span> Colombo</span><br>
-                                <label>Date of journey:</label><span> 2017/04/14</span><br>
-                                <label>Phone number:</label><span> 071-0000000</span><br>
-                            </div>
-                            <div class="col-sm-6">
-                                <label>Departure point:</label><span> Negambo</span><br>
-                                <label>Available Seat(s):</label><span> 2</span><br>
-
-                            </div>
-
-
-                        </div>
-                        <div class="col-sm-4">
-                            <button class="apply_btn">Apply</button>
-                        </div>
-
-
-                    </div>
-                </div>
-            </div>
-
+            <?php if (($results_recurring->num_rows == 0) && ($results_one_time->num_rows == 0)) {
+                // no rides available
+                echo "<div class=\"alert alert-danger\" role=\"alert\">";
+                echo "<strong>Oh Sorry!</strong> No rides available. Come back later...:)";
+                echo "</div>";
+            } else {
+                echo "<div><h3>Available Rides</h3><hr></div><div class=\"panel-group available_rides_panel\"><div class=\"panel panel-default\">";
+            }
+            if ($results_recurring->num_rows > 0) {
+                // display recurring rides
+                echo "<div class=\"panel-heading\">Recurring rides</div>";
+                echo "<div class=\"panel-body\"><div class=\"col-sm-8\"><div class=\"col-sm-6\">";
+                while ($row = $results_recurring->fetch_assoc()) {
+                    echo "<label>Member:</label><span> " . $row["username"] . "</span><br>";
+                    echo "<label>Days:</label>";
+                    // display possible dates
+                    if ($row["monday"] == 1) {
+                        echo "<span>Monday    </span>";
+                    }
+                    if ($row["tuesday"] == 1) {
+                        echo "<span>Tuesday    </span>";
+                    }
+                    if ($row["wednesday"] == 1) {
+                        echo "<span>Wednesday    </span>";
+                    }
+                    if ($row["thursday"] == 1) {
+                        echo "<span>Thursday    </span>";
+                    }
+                    if ($row["friday"] == 1) {
+                        echo "<span>Friday    </span>";
+                    }
+                    if ($row["saturday"] == 1) {
+                        echo "<span>Saturday    </span>";
+                    }
+                    if ($row["sunday"] == 1) {
+                        echo "<span>Sunday    </span>";
+                    }
+                    echo "</<br>";
+                    echo "<label>Available Seat(s):</label><span> " . $row["available_seats"] . "</span><br>";
+                    echo "<label>Phone number:</label><span> " . $row["phone_number"] . "</span><br>";
+                    echo "<label>Comment:</label><span> " . $row["comment"] . "</span><hr>";
+                }
+                echo "</div></div></div>";
+            }
+            if ($results_one_time->num_rows > 0) {
+                // display one time rides
+                echo "<div class=\"panel-heading\">One time rides</div>";
+                echo "<div class=\"panel-body\"><div class=\"col-sm-8\"><div class=\"col-sm-6\">";
+                while ($row = $results_one_time->fetch_assoc()) {
+                    echo "<label>Member:</label><span> " . $row["username"] . "</span><br>";
+                    echo "<label>Date of journey:</label><span> " . $row["date_of_journey"] . "</span><br>";
+                    echo "<label>Available Seat(s):</label><span> " . $row["available_seats"] . "</span><br>";
+                    echo "<label>Phone number:</label><span> " . $row["phone_number"] . "</span><br>";
+                    echo "<label>Comment:</label><span> " . $row["comment"] . "</span><hr>";
+                }
+                echo "</div></div></div>";
+            } ?>
         </div>
     </div>
 
-
-
-
-<?php require_once('footer.php'); ?>
+<?php require_once 'footer.php'; ?>
